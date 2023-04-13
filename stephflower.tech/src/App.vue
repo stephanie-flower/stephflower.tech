95621bf76d29a831f35e1674e2cf2563bb097da2
Stephanie Flower <stephanie.flower.15@gmail.com>
2023-04-13 12:41:31 +0100

    setup

---
+++ stephflower.tech/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Window from './components/Window.vue';
+</script>
+
+<template>
+    <Window>
+      wa wa wee wa
+    </Window>
+</template>
+
+<style scoped>
+
+</style>
---
+++ stephflower.tech/src/components/Window.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    import "98.css";
+</script>
+
+<template>
+    <div class="window">
+        <div class="title-bar">
+            <div class="title-bar-text"> hello world </div>
+            <div class="title-bar-controls">
+                <button aria-label="Minimize"></button>
+                <button aria-label="Maximize"></button>
+                <button aria-label="Close"></button>
+            </div>
+        </div>
+        <div class="window-body">
+                <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style>
+    
+</style>

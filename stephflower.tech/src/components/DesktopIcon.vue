fab073967edc8821f9dce112de8fbc0609a09f72
Stephanie Flower <stephanie.flower.15@gmail.com>
2023-07-20 21:30:23 +0100

    creating active toggle for desktop icons

--- stephflower.tech/src/components/DesktopIcon.vue
+++ stephflower.tech/src/components/DesktopIcon.vue
@@ -1,10 +1,20 @@
 
 <script setup lang="ts">
     import "98.css";
+    import { ref } from "vue";
+
+    var isActive = ref(false);
+
+    const toggleActive = () => {
+        isActive.value = !isActive.value;
+    }
+
 </script>
 
 <template>
-    <div class="icon">
+    <div class="icon"
+    :class="{ 'icon-selected': isActive }"
+    @click="toggleActive">
         <img src="desktopIcons/about.png" alt="About Me">
         <div class="text"> About </div>
     </div>
@@ -22,5 +32,19 @@
         align-items: center;
         padding: 10px;
         user-select: none;
+    }
+
+    .icon-selected {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 10px;
+        user-select: none;
+    }
+
+    .icon-selected .text {
+        border: dotted black 1px;
+        background-color: #000080;
+        color: white;
     }
 </style>
